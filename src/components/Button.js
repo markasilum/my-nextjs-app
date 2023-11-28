8a668bfb98122866bc294fad1bf132a0c1f61137
markasilum <markasilum13@gmail.com>
2023-11-28 18:13:03 +0800

    new

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({onClick, className, text}) => {
   return (
-    <button onClick={onClick} className={`${className} bg-slate-400 px-4 py-2 `}>{text}</button>
+    <button onClick={onClick} className={`${className} bg-slate-400 px-4 py-2 w-40 `}>{text}</button>
   )
 }
 
