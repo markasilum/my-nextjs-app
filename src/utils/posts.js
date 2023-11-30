1c8f098ef9791f52c18d4137471c28cfb13de7df
markasilum <markasilum13@gmail.com>
2023-11-30 20:21:30 +0800

    added database

--- src/utils/posts.js
+++ src/utils/posts.js
@@ -1,6 +1,6 @@
 export default[
-    {id:1, title:'Title 1', author: 'Author 1', body:'body 1'},
-    {id:2, title:'Title 2', author: 'Author 2', body:'body 2'},
-    {id:3, title:'Title 3', author: 'Author 3', body:'body 3'},
-    {id:4, title:'Title 4', author: 'Author 4', body:'body 4'}
+    {title:'Title 1', author: 'Author 1', body:'body 1'},
+    {title:'Title 2', author: 'Author 2', body:'body 2'},
+    {title:'Title 3', author: 'Author 3', body:'body 3'},
+    {title:'Title 4', author: 'Author 4', body:'body 4'}
 ]
